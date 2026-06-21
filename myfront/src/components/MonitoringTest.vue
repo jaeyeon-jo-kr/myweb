f172179e9c0e6abb9f54bcccf19fef7e03b60c94
Jaeyeon Jo <jojaeyeon.kr@gmail.com>
2026-06-21 14:55:44 +0900

    에러 수정

--- myfront/src/components/MonitoringTest.vue
+++ myfront/src/components/MonitoringTest.vue
@@ -7,20 +7,19 @@ const metrics = ref(null)
 // 예시: 엔드포인트가 "/ws-monitoring", 구독 토픽이 "/topic/metrics" 일 때
 const WEBSOCKET_URL = 'ws://localhost:8080/ws-monitoring' 
 const SUBSCRIBE_TOPIC = '/topic/cpu_info'
-
-const connectWebSocket = () => {
-  const stompClient = new Client({
+const stompClient = new Client({
     brokerURL: WEBSOCKET_URL,
     debug: function (str) {
       console.log('[STOMP 디버그]: ' + str) // 통신 오가는 로우 로그 확인용
     },
     reconnectDelay: 5000, // 연결 끊기면 5초마다 자동 재접속 시도
     heartbeatIncoming: 4000,
-    heartbeatOutgoing: 4000,
+    heartbeatOutgoing: 4000
   })
+const connectWebSocket = () => {
 
   // 웹소켓 연결 성공 시 콜백
-  stompClient.onConnect = (frame) => {
+  stompClient.onConnect = () => {
     isConnected.value = true
     console.log('✅ Spring 웹소켓 브로커 연결 성공!')
 
@@ -78,5 +77,5 @@ onUnmounted(() => {
   </div>
 </template>
 
-<script lang="ts" setup>
-</script>
+<style scoped>
+</style>
